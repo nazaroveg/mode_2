42bb79626376fd63a9ee404567fa00d1f0e501da
nazaroveg <nazaroveg@yandex.ru>
2022-07-13 09:29:59 +0300

    v1

---
+++ mode_sub_02.cpp
@@ -0,0 +1,31 @@
+﻿// mode_sub_02.cpp 
+
+#include <iostream>
+
+
+
+
+
+int main()
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+
+   #define SUB(a, b) (a-b)
+	std::cout << SUB(a, b) << std::endl;
+   #undef SUB
+
+    #define SUB(a, b, c) ((a - b) * c)     //a-b = 1 * 2 = 2  (4?)
+	std::cout << SUB(a, b, c) * c << std::endl;
+   #undef SUB
+
+    #define SUB(a, b, c) (((a)-(b)-(c)) *(c))      // a-b-c * c 6-5-2  -1*2 =-2    (-4?)
+	std::cout << SUB(a, b, c ) * c << std::endl; 
+   #undef SUB
+		
+		return 0;
+}
+
+
+
